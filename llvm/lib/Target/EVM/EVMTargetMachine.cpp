matter-labs/era-compiler-llvm

[EVM] Implement reverseBranchCondition and run BranchFolder pass after stackification
This patch adds jump_unless instructions that allows to
implement reverseBranchCondition. This is the same trick
that is used for WebAssembly https://reviews.llvm.org/D14995.
Also it adds one more run of BranchFolder pass after
stackification to optimize branches.

Signed-off-by: Vladimir Radosavljevic <vr@matterlabs.dev>

llvm/lib/Target/EVM/EVMTargetMachine.cpp
@@ -61,6 +61,7 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeEVMTarget() {
   initializeEVMBPStackificationPass(PR);
   initializeEVMAAWrapperPassPass(PR);
   initializeEVMExternalAAWrapperPass(PR);
+  initializeEVMLowerJumpUnlessPass(PR);
 }
 
 static std::string computeDataLayout() {
@@ -186,6 +187,7 @@ public:
   bool addInstSelector() override;
   void addPostRegAlloc() override;
   void addPreEmitPass() override;
+  void addPreEmitPass2() override;
 };
 } // namespace
 
@@ -255,8 +257,16 @@ void EVMPassConfig::addPreEmitPass() {
     } else {
       addPass(createEVMBPStackification());
     }
+
+    // Optimize branch instructions after stackification. This is done again
+    // here, since EVMSplitCriticalEdges may introduce new BBs that could
+    // contain only branches after stackification.
+    if (getOptLevel() != CodeGenOpt::None)
+      addPass(&BranchFolderPassID);
   }
 }
+
+void EVMPassConfig::addPreEmitPass2() { addPass(createEVMLowerJumpUnless()); }
 
 TargetPassConfig *EVMTargetMachine::createPassConfig(PassManagerBase &PM) {
   return new EVMPassConfig(*this, PM);

NomicFoundation/solx-llvm

llvm/lib/Target/EVM/EVMTargetMachine.cpp
@@ -54,6 +54,7 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeEVMTarget() {
   initializeEVMBPStackificationPass(PR);
   initializeEVMAAWrapperPassPass(PR);
   initializeEVMExternalAAWrapperPass(PR);
+  initializeEVMLowerJumpUnlessPass(PR);
 }
 
 static std::string computeDataLayout() {
@@ -179,6 +180,7 @@ public:
   bool addInstSelector() override;
   void addPostRegAlloc() override;
   void addPreEmitPass() override;
+  void addPreEmitPass2() override;
 };
 } // namespace
 
@@ -243,8 +245,16 @@ void EVMPassConfig::addPreEmitPass() {
     addPass(createEVMOptimizeLiveIntervals());
     addPass(createEVMSingleUseExpression());
     addPass(createEVMBPStackification());
+
+    // Optimize branch instructions after stackification. This is done again
+    // here, since EVMSplitCriticalEdges may introduce new BBs that could
+    // contain only branches after stackification.
+    if (getOptLevel() != CodeGenOptLevel::None)
+      addPass(&BranchFolderPassID);
   }
 }
+
+void EVMPassConfig::addPreEmitPass2() { addPass(createEVMLowerJumpUnless()); }
 
 TargetPassConfig *EVMTargetMachine::createPassConfig(PassManagerBase &PM) {
   return new EVMPassConfig(*this, PM);