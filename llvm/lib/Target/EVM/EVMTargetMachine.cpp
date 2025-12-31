matter-labs/era-compiler-llvm

[EVM] Enable Tail Duplication after stackification
In order to do this, the following is done:
1. Remove the call to disablePass(&TailDuplicateID), as we
   want to enable it after stackification.
2. Implement addMachineLateOptimization, to
   explicitly disable TailDuplicate before stackification.
3. Add isNotDuplicable to some stackify instructions.
   Some of them can cause functional issues if
   duplicated (e.g. PUSH_LABEL) and some of them
   can increase code size (e.g. PUSH instructions).

Signed-off-by: Vladimir Radosavljevic <vr@matterlabs.dev>

llvm/lib/Target/EVM/EVMTargetMachine.cpp
@@ -203,6 +203,7 @@ public:
   bool addGCPasses() override { return false; }
   bool addInstSelector() override;
   void addPostRegAlloc() override;
+  void addMachineLateOptimization() override;
   void addPreEmitPass() override;
   void addPreEmitPass2() override;
 };
@@ -252,13 +253,16 @@ void EVMPassConfig::addPostRegAlloc() {
   disablePass(&LiveDebugValuesID);
   disablePass(&PatchableFunctionID);
   disablePass(&ShrinkWrapID);
-  disablePass(&TailDuplicateID);
 
   // TODO: This pass is disabled in WebAssembly, as it hurts code size because
   // it can generate irreducible control flow. Check if this also true for EVM?
   disablePass(&MachineBlockPlacementID);
 
   TargetPassConfig::addPostRegAlloc();
+}
+
+void EVMPassConfig::addMachineLateOptimization() {
+  addPass(&BranchFolderPassID);
 }
 
 void EVMPassConfig::addPreEmitPass() {
@@ -276,8 +280,10 @@ void EVMPassConfig::addPreEmitPass() {
     // Optimize branch instructions after stackification. This is done again
     // here, since EVMSplitCriticalEdges may introduce new BBs that could
     // contain only branches after stackification.
-    if (getOptLevel() != CodeGenOptLevel::None)
+    if (getOptLevel() != CodeGenOptLevel::None) {
       addPass(&BranchFolderPassID);
+      addPass(&TailDuplicateID);
+    }
   }
 }
 

NomicFoundation/solx-llvm

llvm/lib/Target/EVM/EVMTargetMachine.cpp
@@ -203,6 +203,7 @@ public:
   bool addGCPasses() override { return false; }
   bool addInstSelector() override;
   void addPostRegAlloc() override;
+  void addMachineLateOptimization() override;
   void addPreEmitPass() override;
   void addPreEmitPass2() override;
 };
@@ -253,13 +254,16 @@ void EVMPassConfig::addPostRegAlloc() {
   disablePass(&PatchableFunctionID);
   disablePass(&ShrinkWrapID);
   disablePass(&RemoveLoadsIntoFakeUsesID);
-  disablePass(&TailDuplicateLegacyID);
 
   // TODO: This pass is disabled in WebAssembly, as it hurts code size because
   // it can generate irreducible control flow. Check if this also true for EVM?
   disablePass(&MachineBlockPlacementID);
 
   TargetPassConfig::addPostRegAlloc();
+}
+
+void EVMPassConfig::addMachineLateOptimization() {
+  addPass(&BranchFolderPassID);
 }
 
 void EVMPassConfig::addPreEmitPass() {
@@ -277,8 +281,10 @@ void EVMPassConfig::addPreEmitPass() {
     // Optimize branch instructions after stackification. This is done again
     // here, since EVMSplitCriticalEdges may introduce new BBs that could
     // contain only branches after stackification.
-    if (getOptLevel() != CodeGenOptLevel::None)
+    if (getOptLevel() != CodeGenOptLevel::None) {
       addPass(&BranchFolderPassID);
+      addPass(&TailDuplicateLegacyID);
+    }
   }
 }
 