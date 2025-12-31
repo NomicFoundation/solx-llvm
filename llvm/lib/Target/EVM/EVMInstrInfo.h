matter-labs/era-compiler-llvm

[EVM] Adding EVM backend skeleton.
It includes:
  - Description of EVM instructions
  - Instruction selection (ISelLowering, DAGtoDAG)
  - Standard library implementation
  - Initial implementation of MC Layer
  - Initial implementation of AsmPrinter that is able to output virtual registes
  - CI config: compiler-integration-tests.yml.

Signed-off-by: Pavel Kopyl <pavelkopyl@gmail.com>

llvm/lib/Target/EVM/EVMInstrInfo.h
@@ -1,0 +1,55 @@
+//=---------- EVMInstrInfo.h - EVM Instruction Information -*- C++ -*--------=//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the EVM implementation of the
+// TargetInstrInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_EVM_EVMINSTRINFO_H
+#define LLVM_LIB_TARGET_EVM_EVMINSTRINFO_H
+
+#include "EVMRegisterInfo.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
+
+#define GET_INSTRINFO_HEADER
+#include "EVMGenInstrInfo.inc"
+
+namespace llvm {
+
+class EVMInstrInfo final : public EVMGenInstrInfo {
+  const EVMRegisterInfo RI;
+
+public:
+  explicit EVMInstrInfo();
+
+  const EVMRegisterInfo &getRegisterInfo() const { return RI; }
+
+  void copyPhysReg(MachineBasicBlock &MBB, MachineBasicBlock::iterator MI,
+                   const DebugLoc &DL, MCRegister DestReg, MCRegister SrcReg,
+                   bool KillSrc) const override;
+
+  bool analyzeBranch(MachineBasicBlock &MBB, MachineBasicBlock *&TBB,
+                     MachineBasicBlock *&FBB,
+                     SmallVectorImpl<MachineOperand> &Cond,
+                     bool AllowModify) const override;
+
+  unsigned removeBranch(MachineBasicBlock &MBB,
+                        int *BytesRemoved) const override;
+
+  unsigned insertBranch(MachineBasicBlock &MBB, MachineBasicBlock *TBB,
+                        MachineBasicBlock *FBB, ArrayRef<MachineOperand> Cond,
+                        const DebugLoc &DL, int *BytesAdded) const override;
+
+  bool
+  reverseBranchCondition(SmallVectorImpl<MachineOperand> &Cond) const override;
+};
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_EVM_EVMINSTRINFO_H

NomicFoundation/solx-llvm

llvm/lib/Target/EVM/EVMInstrInfo.h
@@ -1,0 +1,56 @@
+//=---------- EVMInstrInfo.h - EVM Instruction Information -*- C++ -*--------=//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the EVM implementation of the
+// TargetInstrInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_EVM_EVMINSTRINFO_H
+#define LLVM_LIB_TARGET_EVM_EVMINSTRINFO_H
+
+#include "EVMRegisterInfo.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
+
+#define GET_INSTRINFO_HEADER
+#include "EVMGenInstrInfo.inc"
+
+namespace llvm {
+
+class EVMInstrInfo final : public EVMGenInstrInfo {
+  const EVMRegisterInfo RI;
+
+public:
+  explicit EVMInstrInfo();
+
+  const EVMRegisterInfo &getRegisterInfo() const { return RI; }
+
+  void copyPhysReg(MachineBasicBlock &MBB, MachineBasicBlock::iterator MI,
+                   const DebugLoc &DL, Register DestReg, Register SrcReg,
+                   bool KillSrc, bool RenamableDest = false,
+                   bool RenamableSrc = false) const override;
+
+  bool analyzeBranch(MachineBasicBlock &MBB, MachineBasicBlock *&TBB,
+                     MachineBasicBlock *&FBB,
+                     SmallVectorImpl<MachineOperand> &Cond,
+                     bool AllowModify) const override;
+
+  unsigned removeBranch(MachineBasicBlock &MBB,
+                        int *BytesRemoved) const override;
+
+  unsigned insertBranch(MachineBasicBlock &MBB, MachineBasicBlock *TBB,
+                        MachineBasicBlock *FBB, ArrayRef<MachineOperand> Cond,
+                        const DebugLoc &DL, int *BytesAdded) const override;
+
+  bool
+  reverseBranchCondition(SmallVectorImpl<MachineOperand> &Cond) const override;
+};
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_EVM_EVMINSTRINFO_H