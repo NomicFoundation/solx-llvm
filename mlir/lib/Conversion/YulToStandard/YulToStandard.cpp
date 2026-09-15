bdad05b2b0bccd5c5083135cdc4afebc409468a3
Abinav Puthan Purayil <abinavpp@gmail.com>
2026-09-15 13:32:02 +0200

    [mlir][Yul] Lower a for step that ends in a leave

--- mlir/lib/Conversion/YulToStandard/YulToStandard.cpp
+++ mlir/lib/Conversion/YulToStandard/YulToStandard.cpp
@@ -1510,13 +1510,15 @@ struct LoopOpInterfaceLowering
         lowerTerminator(bodyYield, (step ? step : cond), r);
     }
 
-    // Lower mandatory step region yield.
+    // Lower step region yield.
     if (step) {
-      auto branch =
-          lowerTerminator(cast<yul::YieldOp>(step->getTerminator()), cond, r);
-      if (op->hasAttr("full_unroll"))
-        branch->setAttr("loop_annotation",
-                        getFullUnrollLoopAnnotation(op->getContext()));
+      // The step has no yield if it ends with a `leave`.
+      if (auto stepYield = dyn_cast<yul::YieldOp>(step->getTerminator())) {
+        auto branch = lowerTerminator(stepYield, cond, r);
+        if (op->hasAttr("full_unroll"))
+          branch->setAttr("loop_annotation",
+                          getFullUnrollLoopAnnotation(op->getContext()));
+      }
     }
 
     // Move region contents out of the loop op.
